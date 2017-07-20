ef60de02c87382d0c063b87586fdfeec4ec44282
yitta <abecc1995@gmail.com>
2017-07-20 16:41:02 +1000

    fix app.js

--- app.js
+++ app.js
@@ -7,42 +7,39 @@ var bodyParser = require('body-parser');
 var todos = require('./routes/todos');
 var AV = require('leanengine');
 
-// 这一段定义了一个路由集合
-
 var wechat = require('./routes/wechatBot');
-
-
-// 加载云函数定义，你可以将云函数拆分到多个文件方便管理，但需要在主文件中加载它们
-require('./cloud');
 
 var app = express();
 
 // 设置模板引擎
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
 app.use(express.static('public'));
 
 // 设置默认超时时间
 app.use(timeout('15s'));
 
+// 加载云函数定义
+require('./cloud');
 // 加载云引擎中间件
 app.use(AV.express());
 
-app.enable('trust proxy');
-// 需要重定向到 HTTPS 可去除下一行的注释。
-// app.use(AV.Cloud.HttpsRedirect());
-
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 app.use(cookieParser());
 
 app.get('/', function(req, res) {
-  res.render('index', { currentTime: new Date() });
+  res.render('index', {
+    currentTime: new Date()
+  });
 });
 
 // 可以将一类的路由单独保存在一个文件中
-app.use('/todos', require('./routes/todos'));
+app.use('/todos', todos);
+// 可以将一类的路由单独保存在一个文件中
+app.use('/wechat', wechat);
 
 app.use(function(req, res, next) {
   // 如果任何一个路由都没有返回响应，则抛出一个 404 异常给后续的异常处理器
@@ -54,7 +51,7 @@ app.use(function(req, res, next) {
 });
 
 // error handlers
-app.use(function(err, req, res, next) {
+app.use(function(err, req, res, next) { // jshint ignore:line
   if (req.timedout && req.headers.upgrade === 'websocket') {
     // 忽略 websocket 的超时
     return;
@@ -69,7 +66,7 @@ app.use(function(err, req, res, next) {
   }
   res.status(statusCode);
   // 默认不输出异常详情
-  var error = {};
+  var error = {}
   if (app.get('env') === 'development') {
     // 如果是开发环境，则将异常堆栈输出到页面，方便开发调试
     error = err;
