8efdb41027a43d71ed36a27f4350468e84ec2ae2
chaorders <thomas.chen.au@gmail.com>
2017-09-20 09:42:52 +1000

    add WP administrator axios login

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 'use strict';
 var AV = require('leanengine');
+const axios = require('axios');
+const { config } = require('./globel');
 
 AV.init({
   appId: process.env.LEANCLOUD_APP_ID,
@@ -20,10 +22,23 @@ app.listen(PORT, function (err) {
   console.log('Node app is running on port:', PORT);
 
   // 注册全局未捕获异常处理器
-  process.on('uncaughtException', function(err) {
+  process.on('uncaughtException', function (err) {
     console.error("Caught exception:", err.stack);
   });
-  process.on('unhandledRejection', function(reason, p) {
+  process.on('unhandledRejection', function (reason, p) {
     console.error("Unhandled Rejection at: Promise ", p, " reason: ", reason.stack);
   });
 });
+
+// 初始化 axios 管理员 token
+const username = process.env.wp_username;
+const password = process.env.wp_password;
+axios.post(`${config.auth_url}`, { username, password })
+  .then(res => {
+    axios.defaults.headers.common['Authorization'] = `Bearer ${res.data.token}`
+    console.log('WP administrator login success.');
+  })
+  .catch(err => {
+    console.log('Fatal error, WP administrator login fail.');
+    console.log(err);
+  })
