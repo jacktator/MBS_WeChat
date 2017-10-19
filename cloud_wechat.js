9add5ba77052ab940ca893725d1954dca9df7c16
Yitta <abecc1995@gmail.com>
2017-10-19 12:28:02 +1100

    test

--- cloud_wechat.js
+++ cloud_wechat.js
@@ -70,18 +70,11 @@ AV.Cloud.define('fetchWeChatOpenId', async (request, response) => {
       // }
     }
     const res = await axios.post(`${config.rest_url}/users`, data)
-    // .then(response => {
-    //   //const resJson = JSON.parse(response.data);
-    //   console.log("res:", response);
-    //   console.log("data", response.data);
-    //   const updateUrl= "https://mbs.sk8tech.io/wp-json/acf/v3/" + response.id;
-    //   console.log("URL##############: ", updateUrl);
-    //   axios.post(updateUrl, { fields: { wechatopenid: openid, accumulatedmbincent: 10000 } })
-    // }).catch(error => console.log("longin error: ",error));
-    const loginRes = await axios.post(`${config.auth_url}`, { username: openid, password: openid });
-    response.success(loginRes.data);
-    const id = loginRes.data.id
-    const updateRes = await axios.post(`${config.acf_url}/users/${id}`, { fields: { wechatopenid: openid, accumulatedmbincent: 10000 } });    
+    const id = res.data.id    
+    const loginRes = await axios.post(`${config.auth_url}`, { username: openid, password: openid }).then(res=>{
+      await axios.post(`${config.acf_url}/users/${id}`, { fields: { wechatopenid: openid, accumulatedmbincent: 10000 } });
+    });
+    response.success(loginRes.data);   
   } catch (err) {
     response.error(err);
   }
